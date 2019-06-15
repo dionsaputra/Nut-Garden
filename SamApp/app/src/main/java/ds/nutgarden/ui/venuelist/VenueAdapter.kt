c13bb5e1efdf9b9842becbf5c1f9a1b1f43080d8
dionsaputra <dionsaputra666@gmail.com>
2019-06-15 15:52:02 +0700

    Add item venue card

--- SamApp/app/src/main/java/ds/nutgarden/ui/venuelist/VenueAdapter.kt
+++ SamApp/app/src/main/java/ds/nutgarden/ui/venuelist/VenueAdapter.kt
@@ -10,7 +10,7 @@ import ds.nutgarden.data.remote.model.Venue
 class VenueAdapter(var data: List<Venue>) : RecyclerView.Adapter<VenueAdapter.VenueHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VenueHolder {
-        return VenueHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_venue, parent, false))
+        return VenueHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_venue_card, parent, false))
     }
 
     override fun getItemCount() = data.size
