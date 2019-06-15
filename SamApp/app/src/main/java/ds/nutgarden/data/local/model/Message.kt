728b5bf0e1aeeb9c36281dbc1113962bf608b2d9
dionsaputra <dionsaputra666@gmail.com>
2019-06-15 14:28:02 +0700

    Add item_chat_date

--- SamApp/app/src/main/java/ds/nutgarden/data/local/model/Message.kt
+++ SamApp/app/src/main/java/ds/nutgarden/data/local/model/Message.kt
@@ -1,9 +1,3 @@
 package ds.nutgarden.data.local.model
 
-data class Message(val message: String): Comparable<Message> {
-
-    override fun compareTo(other: Message): Int {
-
-    }
-
-}
+data class Message(val message: String) {}
